8c088e9eb3c124db09792dd313a11a6ef4b53eac
snehankekre <snehan.minerva@gmail.com>
2024-03-12 10:57:35 +0530

    Replace singleton with cache resource

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ with st.echo():
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
 
-    @st.experimental_singleton
+    @st.cache_resource
     def get_driver():
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
