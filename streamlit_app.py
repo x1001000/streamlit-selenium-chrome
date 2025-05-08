f38c1a6f640614d601098e8b9d0602ce5c9935b8
PHIL <x1001000@gmail.com>
2025-05-08 13:10:52 +0800

    mm chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.get("https://www.macromicro.me/charts/4500/tw-ntd-bond-yield")
 
     st.code(driver.page_source)
